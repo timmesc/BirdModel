2557af67ea23a89580106ad7f9f21cfa048f748f
timmesc <118234989+timmesc@users.noreply.github.com>
2025-01-09 17:27:07 -0500

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', include(('BirdApp.urls', 'BirdApp'), namespace='BirdApp')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
